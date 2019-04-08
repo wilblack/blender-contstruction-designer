6f1b4330eb90d7bf322c0fd392c0e82d5a17b197
Wil Black <wilblack21@gmail.com>
2019-04-08 09:42:16 -0700

    Updating roof color

--- pioneer_trail/house.py
+++ pioneer_trail/house.py
@@ -9,10 +9,19 @@ from lumber_yard.doors import Door, Window
 from lumber_yard.utils import feet
 
 
-DECK_COLOR = (184.0/255, 133.0/255, 31.0/255)
-DECK_HEIGHT = feet(12)
-HOUSE_Y_OFFSET = feet(60)
 
+COLOR_SADDLE = (107.0/255, 78.0/255, 19.0/255)
+COLOR_HOUSE = (163.0/255, 94.0/255, 26.0/255)
+COLOR_ROOF = (15.0/255, 80.0/255, 10.0/255)
+
+HOUSE_Y_OFFSET = feet(60)
+WALL_HEIGHT = feet(20)
+SOUTH_LENGTH = feet(40)
+WEST_LENGTH = feet(27.8)
+
+
+DECK_COLOR = COLOR_SADDLE
+DECK_HEIGHT = feet(12)
 DECK_LENGTH = feet(12.25)
 CURRENT_DECK_WIDTH = feet(19)
 CURRENT_DECK_Y_OFFSET = HOUSE_Y_OFFSET + feet(3)
@@ -34,8 +43,8 @@ STAIRS_Y_OFFSET = STEP_EXTENSION_Y_OFFSET + STEP_EXTENSION_WIDTH
 
 mat_decking = bpy.data.materials.new(name="mat_decking")
 mat2 = bpy.data.materials.new(name="mat2")
-mat3 = bpy.data.materials.new(name="mat3")
-mat4 = bpy.data.materials.new(name="mat4")
+mat_roof = bpy.data.materials.new(name="mat_roof")
+mat_house = bpy.data.materials.new(name="mat_house")
 mat_doors = bpy.data.materials.new(name="mat_doors")
 mat_windows = bpy.data.materials.new(name="mat_windows")
 
@@ -69,25 +78,25 @@ def create_deck(scene):
 
 
 
-    name = 'old_deck'
-    decking = Decking(name, length, CURRENT_DECK_WIDTH, height + feet(0.1))
-    for board in decking.boards:
-        scene.objects.link(board)
-    # Rotate and translate deck
-    bpy.ops.object.select_all(action='DESELECT')
-    objects = bpy.context.scene.objects
-    for obj in objects:
-        label = obj.get('label', None)
-        if label and label == '{0}_2by6'.format(name):
-            print("Selecting {0}".format(label))
-            obj.select = True
-            obj.data.materials.append(mat2)
-            bpy.context.scene.objects.active = obj
-            bpy.context.object.active_material.diffuse_color = (0.6, 0.6, 0.6)
-    bpy.ops.object.join()
-    bpy.ops.transform.translate(value=(DECK_X_OFFSET, CURRENT_DECK_Y_OFFSET, 0))
-    bpy.ops.transform.rotate(value=pi/2, axis=(False, False, True))
-    bpy.context.scene.objects.active = None
+    # name = 'old_deck'
+    # decking = Decking(name, length, CURRENT_DECK_WIDTH, height + feet(0.1))
+    # for board in decking.boards:
+    #     scene.objects.link(board)
+    # # Rotate and translate deck
+    # bpy.ops.object.select_all(action='DESELECT')
+    # objects = bpy.context.scene.objects
+    # for obj in objects:
+    #     label = obj.get('label', None)
+    #     if label and label == '{0}_2by6'.format(name):
+    #         print("Selecting {0}".format(label))
+    #         obj.select = True
+    #         obj.data.materials.append(mat2)
+    #         bpy.context.scene.objects.active = obj
+    #         bpy.context.object.active_material.diffuse_color = (0.6, 0.6, 0.6)
+    # bpy.ops.object.join()
+    # bpy.ops.transform.translate(value=(DECK_X_OFFSET, CURRENT_DECK_Y_OFFSET, 0))
+    # bpy.ops.transform.rotate(value=pi/2, axis=(False, False, True))
+    # bpy.context.scene.objects.active = None
 
 
     # Add step extension
@@ -106,7 +115,6 @@ def create_deck(scene):
             obj.select = True
             obj.data.materials.append(mat_decking)
             bpy.context.scene.objects.active = obj
-            bpy.context.object.active_material.diffuse_color = (0.8, 0.5, 0.5)
 
     bpy.ops.object.join()
     bpy.ops.transform.translate(value=(STEP_EXTENSION_X_OFFSET, STEP_EXTENSION_Y_OFFSET, 0))
@@ -165,9 +173,7 @@ def create_deck(scene):
 
 def create_house(scene):
     location = (0, HOUSE_Y_OFFSET, 0)
-    WALL_HEIGHT = feet(20)
-    SOUTH_LENGTH = feet(40)
-    WEST_LENGTH = feet(27.8)
+
 
 
     SW_CORNER_BOT = Vector((0, 0, 0))
@@ -205,14 +211,7 @@ def create_house(scene):
         NE_CORNER_BOT,
         NE_CORNER_TOP,
         SE_CORNER_TOP,
-        SE_CORNER_BOT,
-
-        # ROOF
-        SW_CORNER_TOP,
-        SE_CORNER_TOP,
-        NE_CORNER_TOP,
-        NW_CORNER_TOP
-
+        SE_CORNER_BOT
     ]
 
     edges = []
@@ -220,17 +219,41 @@ def create_house(scene):
         [0, 1, 2, 3],
         [4, 5, 6, 7],
         [8, 9, 10, 11],
-        [12, 13, 14, 15],
-        [16, 17, 18, 19]
+        [12, 13, 14, 15]
     ]
-
+    # House sides
     mesh = bpy.data.meshes.new(name="South Exterior Wall")
     mesh.from_pydata(verts, edges, faces)
     mesh.validate(verbose=True)
-    obj = bpy.data.objects.new("2by12", mesh)
+    obj = bpy.data.objects.new("house_sides", mesh)
     obj.location = location
     obj["scripted"] = True
     scene.objects.link(obj)
+    obj.data.materials.append(mat_house)
+    bpy.context.scene.objects.active = obj
+    bpy.context.object.active_material.diffuse_color = COLOR_HOUSE
+
+    # House roof
+    verts = [
+        # ROOF
+        SW_CORNER_TOP,
+        SE_CORNER_TOP,
+        NE_CORNER_TOP,
+        NW_CORNER_TOP
+    ]
+    faces = [
+        [0, 1, 2, 3]
+    ]
+    mesh = bpy.data.meshes.new(name="Roof")
+    mesh.from_pydata(verts, edges, faces)
+    mesh.validate(verbose=True)
+    obj = bpy.data.objects.new("roof", mesh)
+    obj.location = location
+    obj["scripted"] = True
+    scene.objects.link(obj)
+    obj.data.materials.append(mat_roof)
+    bpy.context.scene.objects.active = obj
+    bpy.context.object.active_material.diffuse_color = COLOR_ROOF
 
 
     name = 'kitchen_exterior_door'
@@ -259,4 +282,34 @@ def create_house(scene):
     bpy.context.scene.objects.active = window.object
     bpy.context.object.active_material.diffuse_color = (1, 1, 1)
     bpy.ops.transform.translate(value=(DECK_X_OFFSET - DECK_LENGTH, window_y_offset, DECK_HEIGHT + feet(2)))
+    bpy.context.scene.objects.active = None
+
+    name = 'se_large_window_bot'
+    window_y_offset = door_y_offset + feet(8)
+    length = feet(5)
+    height = feet(3)
+    window = Window(name, height=height, length=length)
+    scene.objects.link(window.object)
+    bpy.ops.object.select_all(action='DESELECT')
+    window.object.select = True
+    window.object.data.materials.append(mat_windows)
+    bpy.context.scene.objects.active = window.object
+    bpy.context.object.active_material.diffuse_color = (1, 1, 1)
+    bpy.ops.transform.translate(value=(SOUTH_LENGTH - feet(4), HOUSE_Y_OFFSET - 3, feet(3)))
+    bpy.ops.transform.rotate(value=pi/2, axis=(False, False, True))
+    bpy.context.scene.objects.active = None
+
+    name = 'se_large_window_top'
+    window_y_offset = HOUSE_Y_OFFSET - 3
+    length = feet(5)
+    height = feet(3)
+    window = Window(name, height=height, length=length)
+    scene.objects.link(window.object)
+    bpy.ops.object.select_all(action='DESELECT')
+    window.object.select = True
+    window.object.data.materials.append(mat_windows)
+    bpy.context.scene.objects.active = window.object
+    bpy.context.object.active_material.diffuse_color = (1, 1, 1)
+    bpy.ops.transform.translate(value=(SOUTH_LENGTH - feet(3.25), window_y_offset, DECK_HEIGHT + feet(3)))
+    bpy.ops.transform.rotate(value=pi/2, axis=(False, False, True))
     bpy.context.scene.objects.active = None
